ee90bb11d7c72bff9349386cdebf97070f812a39
FacileAsheesh <asheesh@facileconsulting.com>
2021-03-22 18:17:11 +0530

    File Header, File Layout, Master Sup Dist, Master Sup Item, VIP Chain Code.Vip Class of Trade, VIP Field Values, Vio outlet, Vip Out Let Master, VIP out ownership desc, VIP Out Ownershi Hierarchy Table Added

---
+++ LiquorWarehouseStage_1_0/SRSIn/Tables/VIPOutletCrossReference.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[VIPOutletCrossReference]
+(
+	RECORDTYPE [varchar](6) NOT NULL,
+	DISTID [varchar](25) NOT NULL,
+	RSCUST [varchar](25) NOT NULL,
+	VPID# Numeric(10) NOT NULL,
+	VPST [varchar](2) NOT NULL,
+	VPPARENT [varchar](25)NULL,
+	TDLINXCD [varchar](7)NULL,
+
+)
